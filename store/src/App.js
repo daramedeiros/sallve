743688a515d40a73e8f54e992e053d77c6d67a29
Dara <daraamedeiros@outlook.com>
2021-05-14 00:49:15 -0300

    🔅 Minor fixes

---
+++ store/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { GlobalStyle } from './GlobalStyles';
+
+import Header from './components/Header';
+import ShoppingCart from './containers/ShoppingCart';
+
+function App() {
+
+  return (
+    <>
+    <GlobalStyle />
+      <Header />
+      <ShoppingCart />
+    </>
+  );
+}
+
+export default App;
