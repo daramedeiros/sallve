ed9c233412881e2bf3741e7cbf041af060ee5695
Dara <daraamedeiros@outlook.com>
2021-05-14 12:26:54 -0300

    🔧 Configure environment key

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Header from './components/Header';
 import ShoppingCart from './containers/ShoppingCart';
 
 function App() {
+    
+  fetch(process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1')
+  .then(() => console.log('DEU CERTO'))
+  .catch(() => console.log('DEU ERRO'))
 
   return (
     <>
