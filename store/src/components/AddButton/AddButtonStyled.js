30d12a3705f8b3e2c16b43f8456112564b242df6
Dara <daraamedeiros@outlook.com>
2021-05-14 00:55:00 -0300

    🔅 Minor fixes

---
+++ store/src/components/AddButton/AddButtonStyled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const AddButtonComponent = styled.button`
+    align-content:flex;
+    background: #9f77ff;
+    border: none;
+    color:#ffffff;
+    cursor: pointer;
+    font-size: 20px;
+    font-weight: bold;
+    height: 25px;
+    margin-right: 10px;
+    width: 25px;
+
+    :hover {
+        background: #ccec3a;
+        color: #333333;
+    }
+`;
