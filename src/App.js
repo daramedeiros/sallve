728de445911eab7efcaad011872066effd7597fe
Dara <daraamedeiros@outlook.com>
2021-05-14 12:51:04 -0300

    🔥 Remove netlify

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import Header from './components/Header';
 import ShoppingCart from './containers/ShoppingCart';
 
 function App() {
-    
-  fetch(process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1')
-  .then(() => console.log('DEU CERTO'))
-  .catch(() => console.log('DEU ERRO'))
-
   return (
     <>
     <GlobalStyle />
